8a4830aa842ad85ac5130df484d0039b8cc3d02c
sajad <imsajtj@gmail.com>
2025-01-22 02:16:03 +0330

    final changes

--- config/urls.py
+++ config/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path("schema/", SpectacularAPIView.as_view(api_version="v1"), name="schema"),
     path("", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path("redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
-    path('api/', include(('core.common.urls', 'common'))),
+    path('api/', include(('core.common.urls', 'api'))),
     path('admin/', admin.site.urls),
 ]
